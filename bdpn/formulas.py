eebed902da19d406efb89ac3e030d132e8b2c7bc
Anna Zhukova <zhutchok@gmail.com>
2023-07-27 10:52:57 +0200

    Added comments to formulas

--- bdpn/formulas.py
+++ bdpn/formulas.py
@@ -2,51 +2,102 @@ import numpy as np
 
 
 def get_c1(la, psi, rho):
+    """
+    Return c1 = ((la − psi)^2 + 4 * la * psi * rho)^1/2
+    """
     return np.power(np.power(la - psi, 2) + 4 * la * psi * rho, 0.5)
 
 
 def get_c2(la, psi, c1):
+    """
+    Return c2 = (c1 + la - psi) / (c1 - la + psi)
+    """
     la_minus_psi = la - psi
     return (c1 + la_minus_psi) / (c1 - la_minus_psi)
 
 
 def get_E1(c1, c2, t, T):
+    """
+    Returns E1 = c2 * exp(c1 * (t - T))
+    """
     return c2 * np.exp(c1 * (t - T))
 
 
 def get_E2(c1, c2, ti, T):
+    """
+    Returns E2 = c2 * exp(c1 * (ti - T))
+    """
     return c2 * np.exp(c1 * (ti - T))
 
 
 def get_E3(c1, t, ti):
+    """
+    Returns E3 = exp(c1 * (t - ti))
+    """
     return np.exp(c1 * (t - ti))
 
 
-def get_E4(la, psi, t, ti):
-    return np.exp((la + psi) * (t - ti))
-
-
 def get_u(la, psi, c1, E1):
+    """
+    Returns U(t), where
+    dU(t)/dt = la + psi * U(t) − la U^2(t) − psi * (1 − rho)
+    U(T) = 1
+
+    :param c1: c1 = ((la − psi)^2 + 4 * la * psi * rho)^1/2
+    :param E1: E1 = c2 * exp(c1 * (t - T)), where c2 = (c1 + la - psi) / (c1 - la + psi)
+    """
     two_la = 2 * la
     return (la + psi) / two_la + c1 / two_la * ((E1 - 1) / (E1 + 1))
 
 
 def get_log_p(c1, t, ti, E1, E2):
-    # return np.power((E2 + 1) / (E1 + 1), 2) * E3
-    # return 2 * (np.log(E2 + 1) - np.log(E1 + 1)) + np.log(E3)
+    """
+    Returns log(p(t)), where
+    dp(t)/dt = la + psi * p(t) − 2 * la * p(t) * U(t)
+    p(ti) = 1
+    dU(t)/dt = la + psi * U(t) − la U^2(t) − psi * (1 − rho)
+    U(T) = 1.
+
+    :param c1: c1 = ((la − psi)^2 + 4 * la * psi * rho)^1/2
+    :param E1: E1 = c2 * exp(c1 * (t - T)), where c2 = (c1 + la - psi) / (c1 - la + psi)
+    :param E2: E2 = c2 * exp(c1 * (ti - T))
+    """
     return 2 * (np.log(E2 + 1) - np.log(E1 + 1)) + c1 * (t - ti)
 
 
 def get_log_po(la, psi, c1, t, ti, E1, E2):
-    # return (E2 + 1) / (E1 + 1) * np.power(E3 * E4, 0.5)
-    # return np.log(E2 + 1) - np.log(E1 + 1) + (np.log(E3) + np.log(E4)) / 2
+    """
+    Returns log(po(t)), where
+    dpo(t)/dt = la + psi * po(t) − la * po(t) * U(t)
+    po(ti) = 1
+    dU(t)/dt = la + psi * U(t) − la U^2(t) − psi * (1 − rho)
+    U(T) = 1.
+
+    :param c1: c1 = ((la − psi)^2 + 4 * la * psi * rho)^1/2
+    :param E1: E1 = c2 * exp(c1 * (t - T)), where c2 = (c1 + la - psi) / (c1 - la + psi)
+    :param E2: E2 = c2 * exp(c1 * (ti - T))
+    """
     return np.log(E2 + 1) - np.log(E1 + 1) + (c1 + la + psi) * (t - ti) / 2
 
 
 def get_log_po_from_p_pnh(log_p, log_pnh):
+    """
+    Returns log(po(t)), where
+    dpo(t)/dt = la + psi * po(t) − la * po(t) * U(t)
+    po(ti) = 1
+    if log(p(t)) and log(pnh(t)) are known, where
+    dpnh(t)/dt = la + psi * pnh(t)
+    pnh(ti) = 1,
+    dp(t)/dt = la + psi * p(t) − 2 * la * p(t) * U(t)
+    p(ti) = 1
+    """
     return (log_p + log_pnh) / 2
 
 
 def get_log_pnh(la, psi, t, ti):
-    # return E4
+    """
+    Returns log(pnh(t)), where
+    dpnh(t)/dt = la + psi * pnh(t)
+    pnh(ti) = 1
+    """
     return (la + psi) * (t - ti)
